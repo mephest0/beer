df01bca98d84496e634088fa1dafac48a9d83ded
Conradsen <peter@vev.design>
2021-09-15 22:15:57 +0200

    Added handler for static files and boilerplate for middleware for calls to api/*

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import Koa from 'koa';
+import serve from 'koa-static';
+import { join } from 'path'
 
 const PORT_NUMBER = 31337
 
 console.log('hello world');
 const app = new Koa();
 
-app.use(async (ctx) => {
-	console.log('got request');
-
-	ctx.body = '<html><body><h1>rock on</h1></body></html>';
+// timer-and-error middleware
+app.use(async (ctx, next) => {
+  const time = Date.now()
+  
+  try {
+    await next()
+  } catch (e) {
+    ctx.body = (ctx.body || '') + '<span class="server">there was an error<br />' + (e.message || e.toString()) + '</span>'
+    ctx.status = 400
+  }
+  
+  console.log(`Request to ${ctx.request.path} took ${Date.now() - time}ms`)
 })
 
 app.listen(PORT_NUMBER)
-
-console.log('all set up')
+console.log(`all set up, listening on port ${PORT_NUMBER}`)
