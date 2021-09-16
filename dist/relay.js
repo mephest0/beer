72deba276368e9d2edbf83727bcbbc36c8550c1d
Conradsen <peter@vev.design>
2021-09-16 23:52:33 +0200

    lol

--- dist/relay.js
+++ dist/relay.js
@@ -1,11 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.toggleRelay = void 0;
-const onoff_1 = require("onoff");
+const rpio_1 = __importDefault(require("rpio"));
 const COOLER_RELAY_GPIO = 4;
-const RELAY_CONTROL = new onoff_1.Gpio(COOLER_RELAY_GPIO, 'out');
 const toggleRelay = () => {
-    const state = RELAY_CONTROL.readSync();
-    RELAY_CONTROL.writeSync(state ? 0 : 1);
+    setRelay(!readRelay());
 };
 exports.toggleRelay = toggleRelay;
+const readRelay = () => rpio_1.default.read(COOLER_RELAY_GPIO);
+const setRelay = (state) => {
+    rpio_1.default.write(COOLER_RELAY_GPIO, state ? 1 : 0);
+};
