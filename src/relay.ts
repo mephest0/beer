6642ba9afaa506267b8fa437e553b02fdc04a0d0
Conradsen <peter@vev.design>
2021-09-16 23:58:31 +0200

    Added logging

--- src/relay.ts
+++ src/relay.ts
@@ -2,11 +2,17 @@ import rpio from 'rpio'
 const COOLER_RELAY_GPIO = 4
 
 export const toggleRelay = () => {
+  const nowState = readRelay()
+  console.log('nowState', nowState)
+
   setRelay(!readRelay())
 }
 
 const readRelay = () => rpio.read(COOLER_RELAY_GPIO)
 
 const setRelay = (state: 0 | 1 | boolean) => {
-  rpio.write(COOLER_RELAY_GPIO, state ? 1 : 0)
+  const nuValue = state ? 1 : 0
+  
+  console.log('nuValue', nuValue)
+  rpio.write(COOLER_RELAY_GPIO, nuValue)
 }
